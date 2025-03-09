0370c0a268cb8226bf075b41c03cbe5ddb870d26
Christopher Nadler <christopher.nadler@gmail.com>
2025-03-09 16:26:31 +0100

    bug in cmake corrected (linking problem)

--- src/micropython.cmake
+++ src/micropython.cmake
@@ -6,4 +6,8 @@ target_sources(usermod_mp_face_detect INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/esp_face_detector2.cpp
 )
 
+target_include_directories(usermod_mp_face_detect INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}
+)
+
 target_link_libraries(usermod INTERFACE usermod_mp_face_detect)
