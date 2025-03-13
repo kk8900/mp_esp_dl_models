2994ddfee9007184b55d7103625c47dfe5ca5f8e
Christopher Nadler <147471517+cnadler86@users.noreply.github.com>
2025-03-13 20:12:06 +0100

    micropython.cmake aktualisieren

--- src/micropython.cmake
+++ src/micropython.cmake
@@ -11,4 +11,20 @@ target_include_directories(usermod_mp_face_detect INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
+# Check if MP_JPEG_DIR is set or if mp_jpeg directory exists two levels up
+if(DEFINED MP_JPEG_DIR AND EXISTS "${MP_JPEG_DIR}")
+    message(STATUS "Using user-defined MP_JPEG_DIR: ${MP_JPEG_DIR}")
+    set(MP_JPEG_SRC "${MP_JPEG_DIR}/src/micropython.cmake")
+elseif(EXISTS "${CMAKE_CURRENT_LIST_DIR}/../../mp_jpeg")
+    message(STATUS "Found mp_jpeg directory two levels up")
+    set(MP_JPEG_SRC "${CMAKE_CURRENT_LIST_DIR}/../../mp_jpeg/src/micropython.cmake")
+endif()
+
+# Add MP_JPEG_SRC cmake file to target_sources if it is defined
+if(DEFINED MP_JPEG_SRC AND EXISTS "${MP_JPEG_SRC}")
+    include(${MP_JPEG_SRC})
+else()
+    message(WARNING "MP_JPEG_SRC not found or not defined!")
+endif()
+
 target_link_libraries(usermod INTERFACE usermod_mp_face_detect)
